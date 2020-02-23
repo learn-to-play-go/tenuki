4397640292ceec519e13e47286d117c83aa6acac
Anonymous <>
2020-02-23 20:06:36 +0300

    Center the svg text labels

--- src/svg-renderer.js
+++ src/svg-renderer.js
@@ -285,8 +285,10 @@ const constructSVG = function(renderer, boardState, { hasCoordinates, smallerSto
         utils.appendElement(intersectionInnerContainer, utils.createSVGElement("text", {
           attributes: {
             class: letters[i] + "-label",
-            y: y * (renderer.INTERSECTION_GAP_SIZE + 1) + 5,
-            x: x * (renderer.INTERSECTION_GAP_SIZE + 1) - 7,
+            y: y * renderer.INTERSECTION_GAP_SIZE + y,
+            x: x * renderer.INTERSECTION_GAP_SIZE + x,
+            "text-anchor": "middle",
+            "dominant-baseline": "middle"
           },
           text: letters[i].toUpperCase()
         }));
